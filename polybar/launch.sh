f22f5ec59f224fcc5a9a71bfebb44ee5d429ee50
Mughilan <mughilan16052005@gmail.com>
2021-10-24 20:34:43 +0530

    Updated

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,7 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+# polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
+polybar --config=~/.config/polybar.old/config
 
 echo "Polybar launched..."
